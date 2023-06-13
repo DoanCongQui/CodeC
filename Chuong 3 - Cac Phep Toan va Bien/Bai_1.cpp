a1ed2bd39c47e882a5171d9fc95933dea8d9fd75
DoanCongQui <congt4644@gmail.com>
2023-06-13 22:06:43 +0700

    .

---
+++ Chuong 3 - Cac Phep Toan va Bien/Bai_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+// 1. Cú pháp (syntax):
+//      - toán tử (operator): kí hiệu, kí tự --> toán hạng
+//      - hàm (function): kí tự (đối số)
+// 2. Số toán hạng (operand) tham gia (ngôi)
+// 3. Độ ưu tiên (precedence)
+// 4. Thứ tự thực hiện (asociativity)
+// 5. Giá trị/Kết quả trả về (return value)
+
+int main ()
+{
+    std::cout << sqrt(25) << "\n";
+}
